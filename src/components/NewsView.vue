a444e6b050894c4674dad63bef8830289e8aaddd
Helber Fernandes de França <helberfernandes2000@gmail.com>
2018-08-01 17:19:34 -0300

    Adiciona a classificação

--- src/components/NewsView.vue
+++ src/components/NewsView.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
-      <h1>{{ title }}</h1>
-      <h3>{{ subTitle }}</h3>
+      <h1>{{ news.title }}</h1>
+      <h3>{{ news.subTitle }}</h3>
       <div>
-        {{ url }}
+        {{ news.url }}
         <br/>
         <br/>
-        Data de publicação: {{ datePublished }}
+        Data de publicação: {{ news.datePublished }}
         <br/>
         <br/>
-        {{ content }}
+        {{ news.content }}
+           <br/>
+        <br/>
+        <a href="#"  v-on:click="classifyPositive"> Positive</a>
       </div>
     </div>
 </template>
@@ -21,20 +24,53 @@ export default {
   name: 'home',
   data () {
     return {
-      title:'',
-      subTitle:'',
-      content:'',
+      news: {
+        title:'',
+        subTitle:'',
+        content:'',
+        url:'',
+        datePublished:'',
+        tipo:''
+      },
       url:'',
-      datePublished:''
+      data:''
     }
   },
   methods:{
     fetchData(){
-      this.title = this.$route.params.title
-      this.content = this.$route.params.content
-      this.url = this.$route.params.url
-      this.subTitle = this.$route.params.subTitle
-      this.datePublished = this.$route.params.datePublished
+      this.news.title = this.$route.params.title
+      this.news.content = this.$route.params.content
+      this.news.url = this.$route.params.url
+      this.news.subTitle = this.$route.params.subTitle
+      this.news.datePublished = this.$route.params.datePublished
+    },
+    classifyPositive(){
+      this.url='/api/classify/news'
+      this.news.tipo='Fake'
+      this.data=JSON.stringify({url:this.news.url, tipo:this.news.tipo})
+      console.log(this.data);
+      this.axios      
+        .post(this.url,this.data, {
+                        headers: {
+                            "Content-type": "application/json"
+                        }
+                    }).then(response => {
+                            return response.data;
+                        })
+                        .catch(error => {
+                            this.notify('danger', 'Erro ao obter os dados !', 'ti-close')
+                            console.log(error);
+                        })
+
+    },
+    classifyNegative(){
+      this.axios
+        .post(this.url)
+        .then(response => {
+          this.news = response.data;
+          this.loading=false;
+          })
+        .catch(error => (console.log("error " + error)));
     }
   }
 }
